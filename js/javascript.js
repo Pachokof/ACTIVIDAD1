0bb717e467845ce6229ed959a4a0214a47f2a29c
Pachokof <pachokof25@gmail.com>
2023-06-26 20:47:07 -0500

    Primer carga pagina inicial

---
+++ js/javascript.js
@@ -0,0 +1,70 @@
+    // Variables
+    // Condicionales
+    // Ciclos
+    // Funciones
+
+    // = //Asignar un valor
+    // == // Comparar valores
+    // === // Comparar Valores y Tipos de dato
+
+    1 == 1      // Correcto 
+    "1" == 1    // Correcto
+    1 === 1     // Correcto
+    "1" === 1   // Falso
+
+    1 + 1 = 2 
+    "1" + 1 = 11
+
+
+    if( nombre == "Carlos"){
+        //
+     }
+     
+     if( nombre == "Carlos" ){
+     
+     }else{
+       //
+     }
+     
+     if(Condicion){
+       
+     }else if(Condicion){
+       
+     }else{
+       
+     }
+
+     
+// Variables
+// Condicionales
+// Ciclos
+// Funciones
+
+// Constante
+const pi = 3.1416
+// Global - no se borra y queda memoria 
+var apellidos = "Galvis"
+// Local - Se borra limpia memoria
+let nombre = "Cristhian David"
+
+function name(params) {
+    
+
+}
+
+function name(params) {
+    
+}
+
+var numero1 = 5
+var numero2 = 5
+var numero3 = "5"
+var resultado =  numero1 + numero2 + numero3
+        
+alert( resultado + numero1 )
+
+
+
+
+     
+
